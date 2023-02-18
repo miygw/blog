75ec5924e20c89492b47ce198c632ba60c8b82d2
宮川 <61433095+miygw@users.noreply.github.com>
2023-02-18 23:13:55 +0900

    Feat GitHub Rest API からデータをフェッチする処理

---
+++ src/libs/js/index.ts
@@ -0,0 +1,20 @@
+/** 指定型のオブジェクトとしてフェッチする。 */
+export const fetchObject = async <T extends object>(
+  ...params: Parameters<typeof fetch>
+) => {
+  const raw = await fetchCore(...params);
+  return raw.json() as T;
+};
+
+const fetchCore = async (...params: Parameters<typeof fetch>) => {
+  const resp = await fetch(...params);
+  if (!resp.ok) throw new Error(`${resp.status}: ${resp.statusText}`);
+
+  return resp;
+};
+
+/** サーバーサイドでの実行かを判定する */
+export const isServer = () => typeof window === 'undefined';
+
+/** クライアントサイドでの実行かを判定する */
+export const isClient = () => !isServer();
---
+++ src/libs/node/index.ts
@@ -0,0 +1,29 @@
+import { default as nodeAssert } from 'node:assert';
+
+/** `NODE_ENV` 値が `production` か否かを判断する。 */
+export const isProduction = () => process.env.NODE_ENV === 'production';
+
+/** `NODE_ENV` 値が `development` か否かを判断する。 */
+export const isDevelopment = () => process.env.NODE_ENV === 'development';
+
+/** 環境変数を読み込む。 */
+export const getFromEnv = (key: string) => {
+  const value = process.env[key];
+  assert(value, `.envファイルからキー[${key}]の値を取得できなかった。`);
+
+  return value!;
+};
+
+/** アサーションを実行する。*/
+export const assert = (...params: Parameters<typeof nodeAssert>) => {
+  if (isProduction()) return;
+  nodeAssert(...params);
+};
+
+/** デコードする。 */
+export const decode = (buffer: string, encoding: BufferEncoding | string) => {
+  const bufferEncoding = encoding as BufferEncoding;
+  if (!bufferEncoding) throw new Error(`不明なエンコード方式 ${encoding}`);
+
+  return Buffer.from(buffer, bufferEncoding).toString();
+};

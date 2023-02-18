f17eebbec97095a86c3ca02fa717654519368a58
宮川 <61433095+miygw@users.noreply.github.com>
2023-02-18 23:18:43 +0900

    フェッチ (#5)
    * .envはgitで管理しない
    
    * Feat GitHub Rest API からデータをフェッチする処理

---
+++ src/utils/assert.ts
@@ -0,0 +1,7 @@
+import { isServer } from '@/libs/js';
+import { assert } from '@/libs/node';
+
+/** サーバーサイドでの実行であることをアサートする。 */
+export const assertIsServer = () => {
+  assert(isServer(), 'サーバーサイド以外では実行できない処理が呼び出された。');
+};
---
+++ src/utils/githubRestApi.ts
@@ -0,0 +1,32 @@
+import { fetchObject } from '@/libs/js';
+import { decode, getFromEnv } from '@/libs/node';
+import { assertIsServer } from './assert';
+
+/**
+ * GitHub REST API を利用して、ファイルを取得する。
+ * https://docs.github.com/en/rest/repos/contents#get-repository-content
+ * */
+export const getRepositoryContent = async (path: string) => {
+  assertIsServer();
+
+  const owner = getFromEnv('GITHUB_OWNER');
+  const repo = getFromEnv('GITHUB_REPO');
+  const url = `https://api.github.com/repos/${owner}/${repo}/contents/${path}`;
+
+  type Content = {
+    name: string;
+    encoding: string;
+    content: string;
+  };
+
+  const raw = await fetchObject<Content>(url, {
+    method: 'GET',
+    headers: { Authorization: `Bearer ${getFromEnv('GITHUB_TOKEN')}` },
+  });
+
+  return {
+    name: raw.name,
+    // raw.content は raw.encoding でエンコードされている。
+    content: decode(raw.content, raw.encoding),
+  };
+};
